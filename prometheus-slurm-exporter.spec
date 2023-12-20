4216c25891e6e9af8ca03319f41d805fc0fb836a
Paul Edmon <pedmon@cfa.harvard.edu>
2023-12-20 10:19:09 -0500

    Adding sapphire rapids.

--- prometheus-slurm-exporter.spec
+++ prometheus-slurm-exporter.spec
@@ -1,5 +1,5 @@
 Name:           prometheus-slurm-exporter
-Version:        1.02
+Version:        1.03
 Release:        1%{?dist}
 Summary:        Prometheus Exporter for Slurm
 
--- slurm_cluster_status_collector.py
+++ slurm_cluster_status_collector.py
@@ -84,17 +84,17 @@ class SlurmClusterStatusCollector(Collector):
       ncflops=0
       ngflops=0
       
-      tcpu={'haswell': 0, 'broadwell': 0, 'skylake': 0, 'milan': 0, 'rome': 0, 'cascadelake': 0, 'icelake': 0}
-      ucpu={'haswell': 0, 'broadwell': 0, 'skylake': 0, 'milan': 0, 'rome': 0, 'cascadelake': 0, 'icelake': 0}
+      tcpu={'haswell': 0, 'broadwell': 0, 'skylake': 0, 'milan': 0, 'sapphirerapids': 0, 'rome': 0, 'cascadelake': 0, 'icelake': 0}
+      ucpu={'haswell': 0, 'broadwell': 0, 'skylake': 0, 'milan': 0, 'sapphirerapids': 0, 'rome': 0, 'cascadelake': 0, 'icelake': 0}
       tgpu={'titanx': 0, 'rtx2080ti': 0, 'v100': 0, 'a40': 0, 'a100': 0, 'a100-mig': 0}
       ugpu={'titanx': 0, 'rtx2080ti': 0, 'v100': 0, 'a40': 0, 'a100': 0, 'a100-mig': 0}
-      umem={'haswell': 0, 'broadwell': 0, 'skylake': 0, 'milan': 0, 'rome': 0, 'cascadelake': 0, 'icelake': 0}
+      umem={'haswell': 0, 'broadwell': 0, 'skylake': 0, 'milan': 0, 'sapphirerapids': 0, 'rome': 0, 'cascadelake': 0, 'icelake': 0}
 
       #Current translation from TRES to Double Precision GFLOps
       t2g=93.25
 
       #Current TRES weights
-      wcpu={'haswell': 0.4, 'broadwell': 0.4, 'skylake': 0.5, 'milan': 0.5, 'rome': 0.8, 'cascadelake': 1.0, 'icelake': 1.15}
+      wcpu={'haswell': 0.4, 'broadwell': 0.4, 'skylake': 0.5, 'milan': 0.5, 'sapphirerapids': 0.6, 'rome': 0.8, 'cascadelake': 1.0, 'icelake': 1.15}
       wgpu={'titanx': 2.2, 'rtx2080ti': 75.0, 'v100': 75.0, 'a40': 10.0, 'a100': 209.1, 'a100-mig': 29.9}
 
       #Cycle through each node
@@ -205,11 +205,11 @@ class SlurmClusterStatusCollector(Collector):
 
       #Calculate Total TRES and Total FLOps
       #This is Harvard specific for the weightings.  Update to match what you need.
-      tcputres=float(wcpu['haswell'])*float(tcpu['haswell'])+float(wcpu['broadwell'])*float(tcpu['broadwell'])+float(wcpu['skylake'])*float(tcpu['skylake'])+float(wcpu['milan'])*float(tcpu['milan'])+float(wcpu['rome'])*float(tcpu['rome'])+float(wcpu['cascadelake'])*float(tcpu['cascadelake'])+float(wcpu['icelake'])*float(tcpu['icelake'])
+      tcputres=float(wcpu['haswell'])*float(tcpu['haswell'])+float(wcpu['broadwell'])*float(tcpu['broadwell'])+float(wcpu['skylake'])*float(tcpu['skylake'])+float(wcpu['milan'])*float(tcpu['milan'])+float(wcpu['sapphirerapids'])*float(tcpu['sapphirerapids'])+float(wcpu['rome'])*float(tcpu['rome'])+float(wcpu['cascadelake'])*float(tcpu['cascadelake'])+float(wcpu['icelake'])*float(tcpu['icelake'])
       tmemtres=tcputres
       tgputres=float(wgpu['titanx'])*float(tgpu['titanx'])+float(wgpu['v100'])*float(tgpu['v100'])+float(wgpu['rtx2080ti'])*float(tgpu['rtx2080ti'])+float(wgpu['a40'])*float(tgpu['a40'])+float(wgpu['a100'])*float(tgpu['a100'])+float(wgpu['a100-mig'])*float(tgpu['a100-mig'])
-      ucputres=float(wcpu['haswell'])*float(ucpu['haswell'])+float(wcpu['broadwell'])*float(ucpu['broadwell'])+float(wcpu['skylake'])*float(ucpu['skylake'])+float(wcpu['milan'])*float(ucpu['milan'])+float(wcpu['rome'])*float(ucpu['rome'])+float(wcpu['cascadelake'])*float(ucpu['cascadelake'])+float(wcpu['icelake'])*float(ucpu['icelake'])
-      umemtres=float(wcpu['haswell'])*float(umem['haswell'])+float(wcpu['broadwell'])*float(umem['broadwell'])+float(wcpu['skylake'])*float(umem['skylake'])+float(wcpu['milan'])*float(umem['milan'])+float(wcpu['rome'])*float(umem['rome'])+float(wcpu['cascadelake'])*float(umem['cascadelake'])+float(wcpu['icelake'])*float(umem['icelake'])
+      ucputres=float(wcpu['haswell'])*float(ucpu['haswell'])+float(wcpu['broadwell'])*float(ucpu['broadwell'])+float(wcpu['skylake'])*float(ucpu['skylake'])+float(wcpu['milan'])*float(ucpu['milan'])+float(wcpu['sapphirerapids'])*float(ucpu['sapphirerapids'])+float(wcpu['rome'])*float(ucpu['rome'])+float(wcpu['cascadelake'])*float(ucpu['cascadelake'])+float(wcpu['icelake'])*float(ucpu['icelake'])
+      umemtres=float(wcpu['haswell'])*float(umem['haswell'])+float(wcpu['broadwell'])*float(umem['broadwell'])+float(wcpu['skylake'])*float(umem['skylake'])+float(wcpu['milan'])*float(umem['milan'])+float(wcpu['sapphirerapids'])*float(umem['sapphirerapids'])+float(wcpu['rome'])*float(umem['rome'])+float(wcpu['cascadelake'])*float(umem['cascadelake'])+float(wcpu['icelake'])*float(umem['icelake'])
       ugputres=float(wgpu['titanx'])*float(ugpu['titanx'])+float(wgpu['v100'])*float(ugpu['v100'])+float(wgpu['rtx2080ti'])*float(ugpu['rtx2080ti'])+float(wgpu['a40'])*float(ugpu['a40'])+float(wgpu['a100'])*float(ugpu['a100'])+float(wgpu['a100-mig'])*float(ugpu['a100-mig'])
 
       ttres=tcputres+tmemtres+tgputres
@@ -295,6 +295,7 @@ class SlurmClusterStatusCollector(Collector):
       lsload.add_metric(["tcpubroadwell"],tcpu['broadwell'])
       lsload.add_metric(["tcpuskylake"],tcpu['skylake'])
       lsload.add_metric(["tcpumilan"],tcpu['milan'])
+      lsload.add_metric(["tcpusapphirerapids"],tcpu['sapphirerapids'])
       lsload.add_metric(["tcpurome"],tcpu['rome'])
       lsload.add_metric(["tcpucascadelake"],tcpu['cascadelake'])
       lsload.add_metric(["tcpuicelake"],tcpu['icelake'])
@@ -308,6 +309,7 @@ class SlurmClusterStatusCollector(Collector):
       lsload.add_metric(["ucpubroadwell"],ucpu['broadwell'])
       lsload.add_metric(["ucpuskylake"],ucpu['skylake'])
       lsload.add_metric(["ucpumilan"],ucpu['milan'])
+      lsload.add_metric(["ucpusapphirerapids"],ucpu['sapphirerapids'])
       lsload.add_metric(["ucpurome"],ucpu['rome'])
       lsload.add_metric(["ucpucascadelake"],ucpu['cascadelake'])
       lsload.add_metric(["ucpuicelake"],ucpu['icelake'])
@@ -321,6 +323,7 @@ class SlurmClusterStatusCollector(Collector):
       lsload.add_metric(["umembroadwell"],umem['broadwell'])
       lsload.add_metric(["umemskylake"],umem['skylake'])
       lsload.add_metric(["umemmilan"],umem['milan'])
+      lsload.add_metric(["umemsapphirerapids"],umem['sapphirerapids'])
       lsload.add_metric(["umemrome"],umem['rome'])
       lsload.add_metric(["umemcascadelake"],umem['cascadelake'])
       lsload.add_metric(["umemicelake"],umem['icelake'])
